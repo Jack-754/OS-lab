d94e4757bf6174dcf3cf0f4276f98f65fd2d9315
Aayush <aayushmore754@gmail.com>
2025-01-23 22:13:09 +0530

    working fully

--- LA3/schedule.c
+++ LA3/schedule.c
@@ -2,6 +2,12 @@
 #include <string.h>
 #include <stdlib.h>
 
+//#define DEBUG
+
+// #ifdef DEBUG
+
+// #endif
+
 #define CPU_MODE 0
 #define IO_MODE 1
 #define TIMEOUT_MODE 2
@@ -72,7 +78,8 @@ int front(queue *q){
     return q->start->idx;
 }
 
-typedef struct PCB{
+// Define the PCB structure
+typedef struct PCB {
     int id;
     int arrival;
     int nburst;
@@ -81,22 +88,27 @@ typedef struct PCB{
     int io[9];
     int mode;
     int turnaround;
-    int total;   
-}PCB;
+    int total;
+} PCB;
 
+// Define constants
 #define MAX_SIZE 10000
 
-PCB * comparator;
+// Comparator array for accessing PCB objects
+PCB* comparator;
 
+// Define the MinHeap structure
 typedef struct {
     int data[MAX_SIZE];
     int size;
 } MinHeap;
 
+// Function to initialize the heap
 void initHeap(MinHeap* heap) {
     heap->size = 0;
 }
 
+// Helper functions to calculate parent and children indices
 int parent(int i) {
     return (i - 1) / 2;
 }
@@ -109,12 +121,28 @@ int rightChild(int i) {
     return 2 * i + 2;
 }
 
+// Swap two integers
 void swap(int* a, int* b) {
     int temp = *a;
     *a = *b;
     *b = temp;
 }
 
+// Function to compare two PCBs based on priority rules
+int comparePCB(int idx1, int idx2) {
+    PCB* pcb1 = &comparator[idx1];
+    PCB* pcb2 = &comparator[idx2];
+
+    if (pcb1->arrival != pcb2->arrival) {
+        return pcb1->arrival - pcb2->arrival; // Smaller arrival has higher priority
+    } else if (pcb1->mode != pcb2->mode) {
+        return pcb1->mode - pcb2->mode; // Smaller mode has higher priority
+    } else {
+        return pcb1->id - pcb2->id; // Smaller ID has higher priority
+    }
+}
+
+// Insert a value into the heap
 void insert(MinHeap* heap, int value) {
     if (heap->size == MAX_SIZE) {
         printf("Insert in full heap\n");
@@ -125,11 +153,11 @@ void insert(MinHeap* heap, int value) {
     int i = heap->size;
     heap->size++;
 
+    // Bubble up the inserted value to maintain heap property
     while (i != 0) {
         int parentIndex = parent(i);
-        if (comparator[heap->data[parentIndex]].arrival > comparator[heap->data[i]].arrival || 
-            (comparator[heap->data[parentIndex]].arrival == comparator[heap->data[i]].arrival && heap->data[parentIndex] > heap->data[i])) {
-            swap(&heap->data[parentIndex], &heap->data[i]);
+        if (comparePCB(heap->data[i], heap->data[parentIndex]) < 0) {
+            swap(&heap->data[i], &heap->data[parentIndex]);
             i = parentIndex;
         } else {
             break;
@@ -137,25 +165,20 @@ void insert(MinHeap* heap, int value) {
     }
 }
 
+// Heapify down to maintain heap property
 void heapifyDown(MinHeap* heap, int i) {
     int smallest = i;
     int left = leftChild(i);
     int right = rightChild(i);
 
-    // Compare the left child
-    if (left < heap->size) {
-        if (comparator[heap->data[left]].arrival < comparator[heap->data[smallest]].arrival || 
-            (comparator[heap->data[left]].arrival == comparator[heap->data[smallest]].arrival && heap->data[left] < heap->data[smallest])) {
-            smallest = left;
-        }
+    // Compare left child
+    if (left < heap->size && comparePCB(heap->data[left], heap->data[smallest]) < 0) {
+        smallest = left;
     }
 
-    // Compare the right child
-    if (right < heap->size) {
-        if (comparator[heap->data[right]].arrival < comparator[heap->data[smallest]].arrival || 
-            (comparator[heap->data[right]].arrival == comparator[heap->data[smallest]].arrival && heap->data[right] < heap->data[smallest])) {
-            smallest = right;
-        }
+    // Compare right child
+    if (right < heap->size && comparePCB(heap->data[right], heap->data[smallest]) < 0) {
+        smallest = right;
     }
 
     // Swap and recurse if the smallest is not the current node
@@ -165,7 +188,7 @@ void heapifyDown(MinHeap* heap, int i) {
     }
 }
 
-
+// Extract the minimum value from the heap
 int extractMin(MinHeap* heap) {
     if (heap->size <= 0) {
         printf("Extract from empty heap\n");
@@ -184,9 +207,50 @@ int extractMin(MinHeap* heap) {
     return root;
 }
 
+
 int min(int a, int b){
     if(a<b)return a;
     return b;
+}
+
+void printHeap(MinHeap* heap) {
+    printf("Heap Contents (Size: %d):\n", heap->size);
+    
+    // If heap is empty, print a message
+    if (heap->size == 0) {
+        printf("  Heap is empty\n");
+        return;
+    }
+
+    // Print header for readability
+    printf("  Index\tProcess ID\tArrival Time\n");
+    printf("  -----\t----------\t------------\n");
+
+    // Iterate through all elements in the heap
+    for (int i = 0; i < heap->size; i++) {
+        // Access the process ID stored in the heap
+        int processIndex = heap->data[i];
+
+        // Use comparator to get arrival time and other details
+        printf("  %d\t%d\t\t%d\t%d\n", 
+            i,                                  // Heap index
+            comparator[processIndex].id,        // Process ID
+            comparator[processIndex].arrival,    // Arrival Time
+            comparator[processIndex].mode
+        );
+    }
+    printf("\n");
+}
+
+
+int roundoff(float num) {
+    int int_part = (int)num;
+    if (num - int_part >= 0.5) {
+        return int_part + 1;
+    } else if (num - int_part <= -0.5) {
+        return int_part - 1;
+    }
+    return int_part;
 }
 
 void scheduler(int q, PCB proc[], int n){
@@ -198,31 +262,45 @@ void scheduler(int q, PCB proc[], int n){
     }
     queue * ready = init();
     int time = 0;
-    int STATE = CPU_FREE;
-
+    int STATE = CPU_FREE, EXECUTING;
+    int total_wait=0, total_idle=0, flag=0;
      #ifdef  VERBOSE
-        fprintf(file, "%10d: Starting.\n", time);
+        fprintf(file, "%-10d: Starting.\n", time);
     #endif
     while(event->size>0 || ready->size>0){
         if(event->size>0){
             int nxt = extractMin(event);
+
+            #ifdef DEBUG
+            if(time == 1148)printf("%d, %d, %d\n", proc[nxt].id, proc[nxt].mode, ready->size);
+            #endif
+
             time = proc[nxt].arrival;
+
+            #ifdef DEBUG
+            if(time == 1148){
+                printHeap(event);
+            }
+            #endif
+
             if(proc[nxt].mode == CPU_MODE){
-                if(ready->size==0 )
                 #ifdef  VERBOSE
-                    fprintf(file, "%10d: Process %d joins ready queue.\n", time, proc[nxt].id);
+                    fprintf(file, "%-10d: Process %d joins ready queue.\n", time, proc[nxt].id);
                 #endif
                 enqueue(ready, nxt);
             }
             else if(proc[nxt].mode == IO_MODE){
                 // expecting IO burst but the last cpu burst was completed
                 // printing turnaround time, etc.
-                STATE = CPU_FREE;
+                if(proc[nxt].id == EXECUTING){
+                    STATE = CPU_FREE;
+                }
+
                 if(proc[nxt].nburst-1==proc[nxt].next){
                     proc[nxt].turnaround+=time;
-                    int calc=proc[nxt].turnaround/proc[nxt].total;
-                    calc*=100;
-                    fprintf(file, "%10d :Process %10d exits. Turnaround time = %10d (%4d%%), Wait time = %10d\n", time, proc[nxt].id, proc[nxt].turnaround, calc, proc[nxt].turnaround-proc[nxt].total);
+                    int calc=roundoff((((float)proc[nxt].turnaround)/proc[nxt].total)*100);
+                    total_wait+=proc[nxt].turnaround-proc[nxt].total;
+                    fprintf(file, "%-10d : Process %6d exits. Turnaround time = %5d (%3d%%), Wait time = %d\n", time, proc[nxt].id, proc[nxt].turnaround, calc, proc[nxt].turnaround-proc[nxt].total);
                 }
                 // performing the next IO burst
                 else{
@@ -232,13 +310,20 @@ void scheduler(int q, PCB proc[], int n){
                     // #ifdef  VERBOSE
                     //     fprintf(file, "%10d: Process %d joins ready queue after IO completion.\n", time, proc[nxt].id);
                     // #endif
+
+                    // #ifdef DEBUG
+                    // fprintf(file, "%10d: Process %d will join ready queue after IO completion.  %d\n", time, proc[nxt].id, proc[nxt].arrival);
+                    // #endif
                     insert(event, nxt);
                 }
             }
             else if(proc[nxt].mode == TIMEOUT_MODE){
                 STATE = CPU_FREE;
                 proc[nxt].mode = CPU_MODE;
-                insert(event, nxt);
+                #ifdef  VERBOSE
+                    fprintf(file, "%-10d: Process %d joins ready queue.\n", time, proc[nxt].id);
+                #endif
+                enqueue(ready, nxt);
             }
         }
 
@@ -246,31 +331,47 @@ void scheduler(int q, PCB proc[], int n){
                 int nxt=front(ready);
                 dequeue(ready);
                 STATE = CPU_OCCUPIED;
+                EXECUTING = proc[nxt].id;
+                if(flag==1){
+                    total_idle+=time;
+                    flag=0;
+                }
                 if(proc[nxt].burst[proc[nxt].next]<=q){
                     proc[nxt].mode=IO_MODE;
-                    proc[nxt].arrival+=min(q, proc[nxt].burst[proc[nxt].next]);
+                    proc[nxt].arrival=time+min(q, proc[nxt].burst[proc[nxt].next]);
                     #ifdef  VERBOSE
-                        fprintf(file, "%10d: Process %d scheduled to run for %d (IO).\n", time, proc[nxt].id, min(q, proc[nxt].burst[proc[nxt].next]));
+                        fprintf(file, "%-10d: Process %d scheduled to run for %d (IO).   %d\n", time, proc[nxt].id, min(q, proc[nxt].burst[proc[nxt].next]), proc[nxt].arrival);
                     #endif
                     proc[nxt].burst[proc[nxt].next]=0;
                     insert(event, nxt);
                 }
                 else{
                     proc[nxt].mode=TIMEOUT_MODE;
-                    proc[nxt].arrival+=q;
+                    proc[nxt].arrival=time+q;
                     proc[nxt].burst[proc[nxt].next]-=q;
                     #ifdef  VERBOSE
-                        fprintf(file, "%10d: Process %d scheduled to run for %d  (premepted). \n", time, proc[nxt].id, q);
+                        fprintf(file, "%-10d: Process %d scheduled to run for %d  (premepted).  %d\n", time, proc[nxt].id, q, proc[nxt].arrival);
                     #endif
                     insert(event, nxt);
                 }
         }
-        else{
+        else if(STATE == CPU_FREE){
+            if(flag==0){
+                total_idle-=time;
+                flag=1;
+            }
             #ifdef  VERBOSE
-                fprintf(file, "%10d: CPU goes idle.\n", time);
+                fprintf(file, "%-10d: CPU goes idle.\n", time);
             #endif
         }
     }
+    if(flag==1){
+        total_idle+=time;
+    }
+    fprintf(file, "\nAverage wait time: %.2f\n", ((float)total_wait)/n);
+    fprintf(file, "Total turnaround time: %d\n", time);
+    fprintf(file, "CPU idle time: %d\n", total_idle);
+    fprintf(file, "CPU utilization: %.2f%%\n", (((float)(time-total_idle))/time)*100);
 
 }
 
@@ -312,26 +413,28 @@ int main(){
     #endif
 
     file=fopen(outputfile, "w");
+
     fprintf(file, "**** FCFS scheduling ****\n");
     q=1000000000;
     create_copy(proc, copy, n);
     scheduler(q, copy, n);
 
-    // fprintf(file, "\n\n**** RR scheduling with q = 10 ****\n");
-    // q=10;
-    // create_copy(proc, copy, n);
-    // scheduler(q, copy, n);
+    fprintf(file, "\n\n**** RR scheduling with q = 10 ****\n");
+    q=10;
+    create_copy(proc, copy, n);
+    scheduler(q, copy, n);
 
-    // fprintf(file, "\n\n**** RR scheduling with q = 5 ****\n");
-    // q=5;
-    // create_copy(proc, copy, n);
-    // scheduler(q, copy, n);
+    fprintf(file, "\n\n**** RR scheduling with q = 5 ****\n");
+    q=5;
+    create_copy(proc, copy, n);
+    scheduler(q, copy, n);
     
     // NOTE: TO DO RR SCHEDULING WITH A CUSTOM 'q', UNCOMMENT AND ONLY CHANGE VALUE OF VARIABLE 'q' BELOW
     // q=5;     // SET CUSTOM VALUE OF 'q'
     // fprintf(file, "**** RR scheduling with q = %d ****\n", q);
     // create_copy(proc, copy, n);
     // scheduler(q, copy, n);
+    
     fclose(file);
 
 
